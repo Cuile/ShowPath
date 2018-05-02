4c925b72a1616cfef96ff864eff921f2124406da
cuile <i@cuile.com>
2018-05-02 16:20:52 +0800

    创建项目

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='sequence2hash',
+    version='0.1',
+    description='将 序列值 转为 哈希值',
+    keywords='hash dict list key/value',
+    url='https://github.com/Cuile/sequence2hash',
+
+    packages=['sequence2hash'],
+
+    author='cuile',
+    author_email='i@cuile.com'
+)
