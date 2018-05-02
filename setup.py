c58bc80bcde67e9bdf8cc0eaa1a7bf10dcff5387
cuile <i@cuile.com>
2018-05-02 18:14:09 +0800

    更新项目描述

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 setup(
     name='sequence2hash',
-    version='0.1',
-    description='将 序列值 转为 哈希值',
+    version='1.0.1',
+    description='将 序列值 转为 哈希值，并展示出值在序列中对应的路径',
     keywords='hash dict list key/value',
     url='https://github.com/Cuile/sequence2hash',
 
